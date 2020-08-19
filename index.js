dcb15e5285b38965c21caa821fe2ec8cf57633cb
hardiknai <hardiknai92@gmail.com>
2020-08-19 20:15:11 +0530

    release -2

--- index.js
+++ index.js
@@ -10,31 +10,62 @@ const upload = multer({ dest: "tmp/csv/" });
 const app = express();
 const router = new Router();
 const server = http.createServer(app);
-const dbconnect = require("./dbconnect");
 const Employee = require("./model/Employee");
+const Mongoose = require("mongoose");
 const port = 9000;
 
-app.use("/", dbconnect);
-app.use("/upload-csv", router);
+Mongoose.connect("mongodb://localhost:27017/demo", { useNewUrlParser: true });
 
-router.post("/", upload.single("file"), function (req, res) {
+app.use("/", router);
+
+router.get("/fetchByExperience", function (req, response) {
+  Employee.find([
+    // Grouping pipeline
+    {
+      $group: {
+        _id: "$roomId",
+      },
+    },
+    // Sorting pipeline
+    { $sort: { total_experience: -1 } },
+    // Optionally limit results
+    { $limit: 5 },
+  ]).then((employee) => {
+    if (employee) response.send(employee);
+    else response.error(error);
+  });
+});
+router.get("/fetchByExperience", function (req, response) {
+  Employee.find({}).then((employee) => {
+    if (employee) response.send(employee);
+    else response.error(error);
+  });
+});
+router.get("/fetchByEfficiencyRange", function (req, response) {
+  Employee.find({}).then((employee) => {
+    if (employee) response.send(employee);
+    else response.error(error);
+  });
+});
+
+router.post("/upload-csv", upload.single("file"), function (req, res) {
   const fileRows = [];
   csv
     .parseFile(req.file.path)
     .on("data", function (data) {
       fileRows.push(data);
-      const { name, salary, efficiency, total_experience } = data;
+
+      let image_path = "./images/" + Date.now() + ".jpg";
       const newEmployee = new Employee({
-        name,
-        salary,
-        efficiency,
-        total_experience,
+        name: data[0],
+        salary: data[1],
+        efficiency: data[2],
+        total_experience: data[3],
+        image: image_path,
       });
       newEmployee.save().then((storedEmployee) => {
         console.log(storedEmployee._id);
       });
-
-      console.log(data); // push each row
     })
     .on("end", function () {
       fs.unlinkSync(req.file.path); // remove temp file
